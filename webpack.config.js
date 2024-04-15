fa53f71dd818a39a2514c77d460b07f30f5c671f
Venkat408 <venkat408prabhu@gmail.com>
2024-04-15 20:47:56 +0530

    netlify to test payment gatway v2 -65 -- 15-04-24

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './netlify/edge-functions/hello.js', // Path to your edge function file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  target: 'node',
+};
