80fa53ecb082d2584bd2caa6205403b1cc15e07b
Venkat408 <venkat408prabhu@gmail.com>
2024-04-15 17:53:01 +0530

    netlify to test payment gatway v2 -37 -- 15-04-24

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,3 @@
-import { HmacSHA256, enc } from 'crypto-js';
-
 function urlBase64Encode(str) {
     let base64 = btoa(unescape(encodeURIComponent(str)));
     const padding = '='.repeat((4 - base64.length % 4) % 4);
@@ -7,9 +5,53 @@ function urlBase64Encode(str) {
 }
 export default async (req) => {
     try {
-        console.log('hello world');
+        const clientID = "tech234sdf";
+        const secretKey = "YugsdGsdifugHGasgsd";
+      
+        const jwsHeader = JSON.stringify({
+          "alg": "HS256",
+          "clientid": clientID
+        });
+      
+        const jwsPayload = JSON.stringify({
+          "mercid": "TECH234SDF",
+          "orderid": "order45608988",
+          "amount": "300.00",
+          "order_date": "2023-07-16T10:59:15+05:30",
+          "currency": "356",
+          "ru": "https://www.merchant.com/",
+          "additional_info": {
+            "additional_info1": "Details1",
+            "additional_info2": "Details2"
+          },
+          "itemcode": "DIRECT",
+          "device": {
+            "init_channel": "internet",
+            "ip": "103.104.59.11",
+            "user_agent": "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101 Firefox/51.0",
+            "accept_header": "text/html",
+            "fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
+            "browser_tz": "-330",
+            "browser_color_depth": "32",
+            "browser_java_enabled": "false",
+            "browser_screen_height": "601",
+            "browser_screen_width": "657",
+            "browser_language": "en-US",
+            "browser_javascript_enabled": "true"
+          }
+        });
+        const unsignedToken = `${urlBase64Encode(jwsHeader)}.${urlBase64Encode(jwsPayload)}`;
 
-        return new Response(JSON.stringify({'status':'success' }), {
+        // const signature = HmacSHA256(unsignedToken, secretKey);
+        // console.log(signature);
+
+        // const base64UrlSignature = this.urlBase64Encode(this.utf8Encode(signature.toString(enc.Latin1))); // Use Latin1 encoding
+        
+        // const jwsToken = `${urlBase64Encode(jwsHeader)}.${urlBase64Encode(jwsPayload)}.${base64UrlSignature}`;
+      
+
+
+        return new Response(JSON.stringify({'status':'success', 'jwsToken':unsignedToken }), {
             headers: { "Content-Type": "application/json" },
         });
     }
