54f6e0668b9ea2427929847016369fa8424423b0
Venkat408 <venkat408prabhu@gmail.com>
2024-04-15 16:34:56 +0530

    netlify to test payment gatway v2 -18 -- 15-04-24

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,8 @@
+function urlBase64Encode(str) {
+    let base64 = btoa(unescape(encodeURIComponent(str)));
+    const padding = '='.repeat((4 - base64.length % 4) % 4);
+    return (base64 + padding).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
 export default async (req) => {
     try {
         const clientID = "tech234sdf";
