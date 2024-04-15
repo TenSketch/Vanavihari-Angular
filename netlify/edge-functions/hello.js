6240f56828ae0ac98a79b700ed0112ac22f696f1
Venkat408 <venkat408prabhu@gmail.com>
2024-04-15 16:31:51 +0530

    netlify to test payment gatway v2 -16 -- 15-04-24

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,43 @@
 export default async (req) => {
     try {
-        console.log('test');
-        return new Response(JSON.stringify({'status':'success'}), {
+        const clientID = "tech234sdf";
+        const secretKey = "YugsdGsdifugHGasgsd";
+      
+        const jwsHeader = JSON.stringify({
+          "alg": "HS256",
+          "clientid": clientID
+        });
+      
+        const jwsPayload = JSON.stringify({
+          "mercid": "TECH234SDF",
+          "orderid": "order45608988",
+          "amount": "300.00",
+          "order_date": "2023-07-16T10:59:15+05:30",
+          "currency": "356",
+          "ru": "https://www.merchant.com/",
+          "additional_info": {
+            "additional_info1": "Details1",
+            "additional_info2": "Details2"
+          },
+          "itemcode": "DIRECT",
+          "device": {
+            "init_channel": "internet",
+            "ip": "103.104.59.11",
+            "user_agent": "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101 Firefox/51.0",
+            "accept_header": "text/html",
+            "fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
+            "browser_tz": "-330",
+            "browser_color_depth": "32",
+            "browser_java_enabled": "false",
+            "browser_screen_height": "601",
+            "browser_screen_width": "657",
+            "browser_language": "en-US",
+            "browser_javascript_enabled": "true"
+          }
+        });
+      
+        const base64UrlHeader = urlBase64Encode(jwsHeader);
+        return new Response(JSON.stringify({'status':'success', 'base64UrlHeader':base64UrlHeader}), {
             headers: { "Content-Type": "application/json" },
           });
     }
