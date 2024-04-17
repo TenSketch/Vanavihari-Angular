36e5ef363809026e39769b1bf2c70cc108ba47ef
Venkat408 <venkat408prabhu@gmail.com>
2024-04-17 22:27:18 +0530

    try jose in hello.js -3 -- 17-04-24

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,10 +1,10 @@
-// import * as jose from 'node:jose';
+import { CompactSign, jwtVerify } from 'jose';
 
-// function urlBase64Encode(str) {
-//     let base64 = btoa(unescape(encodeURIComponent(str)));
-//     const padding = '='.repeat((4 - base64.length % 4) % 4);
-//     return (base64 + padding).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
-// }
+function urlBase64Encode(str) {
+    let base64 = btoa(unescape(encodeURIComponent(str)));
+    const padding = '='.repeat((4 - base64.length % 4) % 4);
+    return (base64 + padding).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
 // function calculateHmacSha256(data, key) {
 //   const dataUint8 = new TextEncoder().encode(data);
 //   const keyUint8 = new TextEncoder().encode(key);
@@ -18,48 +18,56 @@
 // }
 export default async (req) => {
     try {
-        const clientID = "tech234sdf";
-        const secretKey = "YugsdGsdifugHGasgsd";
-      
-        const jwsHeader = JSON.stringify({
-          "alg": "HS256",
-          "clientid": clientID
-        });
-      
-        const jwsPayload = JSON.stringify({
-          "mercid": "TECH234SDF",
-          "orderid": "order45608988",
-          "amount": "300.00",
-          "order_date": "2023-07-16T10:59:15+05:30",
-          "currency": "356",
-          "ru": "https://www.merchant.com/",
-          "additional_info": {
-            "additional_info1": "Details1",
-            "additional_info2": "Details2"
-          },
-          "itemcode": "DIRECT",
-          "device": {
-            "init_channel": "internet",
-            "ip": "103.104.59.11",
-            "user_agent": "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101 Firefox/51.0",
-            "accept_header": "text/html",
-            "fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
-            "browser_tz": "-330",
-            "browser_color_depth": "32",
-            "browser_java_enabled": "false",
-            "browser_screen_height": "601",
-            "browser_screen_width": "657",
-            "browser_language": "en-US",
-            "browser_javascript_enabled": "true"
-          }
-        });
-        // const unsignedToken = `${urlBase64Encode(jwsHeader)}.${urlBase64Encode(jwsPayload)}`;
-        // const signature = calculateHmacSha256(unsignedToken, secretKey);
-        // const base64UrlSignature = urlBase64Encode(signature);
-        // const jwsToken = `${urlBase64Encode(jwsHeader)}.${urlBase64Encode(jwsPayload)}.${base64UrlSignature}`;
-        return new Response(JSON.stringify({'status':'success', 'jwsPayload':jwsPayload }), {
-            headers: { "Content-Type": "application/json" },
-        });
+      const clientID = "bduatv2apt";
+      const secretKey = "DnVd1pJpk3oFOdjNgRRPT1OgwfH1DYku";
+  
+      const secretKeyUint8 = new TextEncoder().encode(secretKey);
+      const jwk = {
+        kty: 'oct',
+        alg: 'HS256',
+        k: secretKeyUint8
+      };
+  
+      const jwsHeader = {
+        "alg": "HS256",
+        "clientid": clientID
+      };
+  
+      const jwsPayload = {
+        "mercid": "BDUATV2APT",
+        "orderid": "order45608988",
+        "amount": "300.00",
+        "order_date": "2023-07-16T10:59:15+05:30",
+        "currency": "356",
+        "ru": "https://vanavihari.com/#/payment-return",
+        "additional_info": {
+          "additional_info1": "Details1",
+          "additional_info2": "Details2"
+        },
+        "itemcode": "DIRECT",
+        "device": {
+          "init_channel": "internet",
+          "ip": "103.104.59.11",
+          "user_agent": "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101 Firefox/51.0",
+          "accept_header": "text/html",
+          "fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
+          "browser_tz": "-330",
+          "browser_color_depth": "32",
+          "browser_java_enabled": "false",
+          "browser_screen_height": "601",
+          "browser_screen_width": "657",
+          "browser_language": "en-US",
+          "browser_javascript_enabled": "true"
+        }
+      };
+  
+      const jwsPayloadUint8 = new TextEncoder().encode(jwsPayload);
+      const jwsToken = await new CompactSign(jwsPayloadUint8)
+        .setProtectedHeader(jwsHeader)
+        .sign(jwk);
+      return new Response(JSON.stringify({'status':'success', 'jws':jws }), {
+          headers: { "Content-Type": "application/json" },
+      });
     }
     catch (error)
     {
