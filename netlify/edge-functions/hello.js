7723319c20c0fbf650c5121470b27bb4358b9bdc
Venkat408 <venkat408prabhu@gmail.com>
2024-04-18 18:02:26 +0530

    try jose in hello.js -5 -- 18-04-24

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,7 @@
 import { CompactSign, jwtVerify } from 'jose';
 
 function urlBase64Encode(str) {
-    let base64 = btoa(unescape(encodeURIComponent(str)));
+    let base64 = btoa(encodeURIComponent(str));
     const padding = '='.repeat((4 - base64.length % 4) % 4);
     return (base64 + padding).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
@@ -32,40 +32,76 @@ export default async (req) => {
         "alg": "HS256",
         "clientid": clientID
       };
-  
       const jwsPayload = {
-        "mercid": "BDUATV2APT",
+        "mercid": "test",
         "orderid": "order45608988",
         "amount": "300.00",
         "order_date": "2023-07-16T10:59:15+05:30",
         "currency": "356",
-        "ru": "https://vanavihari.com/#/payment-return",
+        "ru": "https://www.merchant.com/",
         "additional_info": {
-          "additional_info1": "Details1",
-          "additional_info2": "Details2"
+            "additional_info1": "Details1",
+            "additional_info2": "Details2"
         },
         "itemcode": "DIRECT",
         "device": {
-          "init_channel": "internet",
-          "ip": "103.104.59.11",
-          "user_agent": "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101 Firefox/51.0",
-          "accept_header": "text/html",
-          "fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
-          "browser_tz": "-330",
-          "browser_color_depth": "32",
-          "browser_java_enabled": "false",
-          "browser_screen_height": "601",
-          "browser_screen_width": "657",
-          "browser_language": "en-US",
-          "browser_javascript_enabled": "true"
+            "init_channel": "internet",
+            "ip": "103.104.59.11",
+            "user_agent": "Mozilla/5.0(WindowsNT10.0;WOW64;rv:51.0)Gecko/20100101 Firefox/51.0",
+            "accept_header": "text/html",
+            "fingerprintid": "61b12c18b5d0cf901be34a23ca64bb19",
+            "browser_tz": "-330",
+            "browser_color_depth": "32",
+            "browser_java_enabled": "false",
+            "browser_screen_height": "601",
+            "browser_screen_width": "657",
+            "browser_language": "en-US",
+            "browser_javascript_enabled": "true"
         }
-      };
-  
+    };
+    
+    // const base64UrlPayload = Buffer.from(JSON.stringify(jwsPayload)).toString('base64');
       const jwsPayloadUint8 = new TextEncoder().encode(jwsPayload);
       const jwsToken = await new CompactSign(jwsPayloadUint8)
         .setProtectedHeader(jwsHeader)
-        .sign(jwk);
-      return new Response(JSON.stringify({'status':'success', 'jws':jws }), {
+        .sign(new TextEncoder().encode(jwk));
+
+
+
+        const apiUrl = "https://uat1.billdesk.com/u2/payments/ve1_2/orders/create";
+        const headers = {
+            "Content-Type": "application/jose",
+            "Accept": "application/jose",
+            "BD-Traceid": "20201817132207ABD1K",
+            "BD-Timestamp": `${Math.floor(Date.now() / 1000)}`
+        };
+        const options = {
+            method: 'POST',
+            body: jwsToken, // Assuming jwsToken is your payload
+            headers: headers
+        };
+
+        fetch(apiUrl, options)
+        .then(response => {
+            console.log('Response status:', response.status);
+            return response.json();
+        })
+        .then(data => {
+            console.log('Response data:', data);
+
+            return new Response(JSON.stringify({'status':'success', 'data':data }), {
+                headers: { "Content-Type": "application/json" },
+            });
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    
+
+
+
+            
+      return new Response(JSON.stringify({'status':'success', 'jws':base64UrlPayload }), {
           headers: { "Content-Type": "application/json" },
       });
     }
