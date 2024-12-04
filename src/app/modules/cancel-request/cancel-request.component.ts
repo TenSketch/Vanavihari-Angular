84a28459c801515bf48df2fc5c463d424a699d72
venkat408 <venkat408prabhu@gmail.com>
2024-12-04 19:03:03 +0530

    fix refund amount 3

--- src/app/modules/cancel-request/cancel-request.component.ts
+++ src/app/modules/cancel-request/cancel-request.component.ts
@@ -86,10 +86,7 @@ export class CancelRequestComponent {
   confirmCancellation() {
     // Logic to handle the cancellation request submission
     this.showLoader = true;
-    const refundableAmount = this.calculateAmount(
-      this.getCheckInDate(),
-      this.totalAmount
-    ).toFixed(2);
+    const refundableAmount = parseFloat(this.calculateAmount(this.getCheckInDate(), this.totalAmount).toString()).toFixed(2);
     const formattedDateTimeStr = this.getformattedDateTimeStr();
     const uniqueKey = this.generateUniqueKey();
     const Payment_Transaction_Date = this.getpaymentTransactionDate();
