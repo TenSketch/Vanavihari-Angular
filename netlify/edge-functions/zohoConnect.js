0e95e50de06e5ab03d49adda48ebbb5e3848de0f
venkat408 <venkat408prabhu@gmail.com>
2024-12-23 14:11:43 +0530

    test quary api 15

--- netlify/edge-functions/zohoConnect.js
+++ netlify/edge-functions/zohoConnect.js
@@ -525,12 +525,12 @@ export default async (req) => {
         break;
       case "query_api":
         const nowTime = new Date();
-        const year = now.getFullYear();
-        const month = String(now.getMonth() + 1).padStart(2, '0');
-        const day = String(now.getDate()).padStart(2, '0');
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const year = nowTime.getFullYear();
+        const month = String(nowTime.getMonth() + 1).padStart(2, '0');
+        const day = String(nowTime.getDate()).padStart(2, '0');
+        const hours = String(nowTime.getHours()).padStart(2, '0');
+        const minutes = String(nowTime.getMinutes()).padStart(2, '0');
+        const seconds = String(nowTime.getSeconds()).padStart(2, '0');
         const formattedDateTime = `${year}${month}${day}${hours}${minutes}${seconds}`;
 
 
