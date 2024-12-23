26ad018eb55ab610d0bb21aa7747bbc9e31bf974
venkat408 <venkat408prabhu@gmail.com>
2024-12-23 14:09:04 +0530

    test quary api 14

--- netlify/edge-functions/zohoConnect.js
+++ netlify/edge-functions/zohoConnect.js
@@ -525,8 +525,17 @@ export default async (req) => {
         break;
       case "query_api":
         const nowTime = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const formattedDateTime = `${year}${month}${day}${hours}${minutes}${seconds}`;
+
+
         const bodyText11 =  queryParams.get("test");
-        const checksum = "0122|VANAVIHARI|"+bodyText11+"|"+(nowTime.toISOString().slice(0, 19).replace('T', ' '));
+        const checksum = "0122|VANAVIHARI|"+bodyText11+"|"+formattedDateTime;
         const hmacsha256 = createHmac("sha256", process.env.Billdesk_SecretKey)
         .update(checksum)
         .digest("hex")
